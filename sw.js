3f64c728f8a34ca21957e986bd0e012aed0c4745
João <joaopedrocolmanette@gmail.com>
2026-09-24 15:59:45 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='financas-clinica-v9';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch', event => {
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    fetch(event.request).then(resp=>{
+      if(resp && resp.ok) caches.open(CACHE).then(cache=>cache.put(event.request, resp.clone()));
+      return resp;
+    }).catch(()=>caches.match(event.request))
+  );
+});
